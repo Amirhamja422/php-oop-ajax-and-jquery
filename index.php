3ce08807ec3380364c7880d78bd52b0149b4de69
Sakil Mahmud <sakil.diu.cse@gmail.com>
2022-11-22 00:04:17 +0600

    corrected spilling mistake on index.php file

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
-<?php 
-include 'header.php'; 
+<?php
+include 'header.php';
 ?>
-<h2>Topics: Poject List</h2>
+<h2>Topics: Project List</h2>
 
 <div class="content">
-	<div class="topics">
-		<ul>
-			<li><a href="user.php" target="_blank"></a>check username availability</li>
-		</ul>
-</div>
+    <div class="topics">
+        <ul>
+            <li><a href="user.php" target="_blank"></a>check username availability</li>
+        </ul>
+    </div>
 </div>
 
 <?php include 'footer.php'; ?>
